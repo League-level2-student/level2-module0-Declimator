3d8fefabecb3663417c9986d57ab39145425046e
Declimator <league@WTS13.attlocal.net>
2018-12-06 17:00:22 -0800

    ArrayCheatSheet, RobotRace IP

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,43 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String[] s = {"1", "2", "3", "4", "5"};
 		//2. print the third element in the array
-		
+		System.out.println(s[2]);
 		//3. set the third element to a different value
-		
+		s[2] = "something";
 		//4. print the third element again
-		
+		System.out.println(s[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
-		
+		for(int i = 0; i<s.length;i++) {
+			System.out.println(s[i]);
+		}
+		System.out.println("___________________________________________________________________");
 		//6. make an array of 50 integers
-		
+		int[] x = new int[50];
 		//7. use a for loop to make every value of the integer array a random number
-		
+
+		for(int i = 0;i<x.length;i++) {
+			x[i] = new Random().nextInt(50);
+
+		}
+		int y = x[0];
+		for(int i = 0;i<x.length;i++) {
+			if(y>x[i]) {
+				y = x[i];
+			}
+		}
 		//8. without printing the entire array, print only the smallest number in the array
-		
+		System.out.println(y);
 		//9 print the entire array to see if step 8 was correct
-		
+		for(int i = 0; i<x.length;i++) {
+		System.out.println(x[i]);
+		}
 		//10. print the largest number in the array.
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,83 @@
 package arrays;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
-	//1. make a main method
+	// 1. make a main method
+	public static void main(String[] args) {
 
-	//2. create an array of 5 robots.
+		// 2. create an array of 5 robots.
+Robot x[]= new Robot[5];
+		// 3. use a for loop to initialize the robots.
+for(int i = 0;i<x.length;i++) {
+	x[i] = new Robot("mini");
+}
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
+for(int i = 0;i<x.length;i++) {
+	x[i].setSpeed(100);
+	x[i].penDown();
+	x[i].moveTo(20*i +50, 250);
+}
+boolean isOver = false;
+boolean firstTurn = false;
+boolean first = false;
+boolean second = false;
+boolean third = false;
+boolean fourth = false;
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
+int count = 1;
+int o = 250;
+int slowest = 0;
+while(firstTurn == false) {
+for(int i = 0;i<x.length;i++) {
+	int z = new Random().nextInt(50)+1;
+	x[i].move(z);
+	if(x[i].getY()<=20*i) {
+		x[i].turn(90);
+		if(count==1) {
+			first=true;
+			i=10;
+		}
+		else if(count==2) {
+			second=true;
+			i=11;
+		}
+		else if(count==3) {
+			third=true;
+			i=12;
+		}
+		else if (count==4) {
+			fourth=true;
+			i=13;
+		}
+		count++;
+	}
+	if(o<x[i].getY()) {
+		o=x[i].getX();
+		slowest = i;
+	}
+	if(o<=20*slowest) {
+		firstTurn = true;
+	}
+	if(x[i].getY()>=249) {
+		i++;
+		JOptionPane.showMessageDialog(null, "Robot " + i + " has won!");
+		isOver = true;
+	}
+}
+}
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		// screen.
 
-	//3. use a for loop to initialize the robots.
+		// 7. declare that robot the winner and throw it a party!
 
-		//4. make each robot start at the bottom of the screen, side by side, facing up
+		// 8. try different races with different amounts of robots.
 
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
-
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
-	//7. declare that robot the winner and throw it a party!
-
-	//8. try different races with different amounts of robots.
-
-	//9. make the robots race around a circular track.
+		// 9. make the robots race around a circular track.
+	}
 }
