e03f1ff93bf74f1d502d0a0c554acc7f3a718be8
Declimator <league@WTS22.attlocal.net>
2018-12-13 16:56:44 -0800

    RobotRace, FindTheHiddenButton IP

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -2,8 +2,6 @@ package arrays;
 
 import java.util.Random;
 
-import javax.swing.JOptionPane;
-
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
@@ -11,66 +9,111 @@ public class _01_RobotRace {
 	public static void main(String[] args) {
 
 		// 2. create an array of 5 robots.
-Robot x[]= new Robot[5];
+		Robot x[] = new Robot[5];
 		// 3. use a for loop to initialize the robots.
-for(int i = 0;i<x.length;i++) {
-	x[i] = new Robot("mini");
-}
+		for (int i = 0; i < x.length; i++) {
+			x[i] = new Robot("mini");
+		}
 		// 4. make each robot start at the bottom of the screen, side by side, facing up
-for(int i = 0;i<x.length;i++) {
-	x[i].setSpeed(100);
-	x[i].penDown();
-	x[i].moveTo(20*i +50, 250);
-}
-boolean isOver = false;
-boolean firstTurn = false;
-boolean first = false;
-boolean second = false;
-boolean third = false;
-boolean fourth = false;
+		for (int i = 0; i < x.length; i++) {
+			x[i].setSpeed(300);
+			x[i].penDown();
+			x[i].moveTo(20 * i + 50, 250);
+		}
+		boolean isOver = false;
+		boolean firstTurn = false;
+		boolean secondTurn = false;
+		boolean thirdTurn = false;
+		boolean fourthTurn = false;
+		boolean pass = false;
+		int number[] = { 0, 0, 0, 0, 0 };
+
 		// 5. use another for loop to iterate through the array and make each robot move
 		// a random amount less than 50.
-int count = 1;
-int o = 250;
-int slowest = 0;
-while(firstTurn == false) {
-for(int i = 0;i<x.length;i++) {
-	int z = new Random().nextInt(50)+1;
-	x[i].move(z);
-	if(x[i].getY()<=20*i) {
-		x[i].turn(90);
-		if(count==1) {
-			first=true;
-			i=10;
+		int count = 0;
+		int o = 0;
+		int slowest = 0;
+		while (firstTurn == false) {
+			for (int i = 0; i < x.length; i++) {
+				int z = new Random().nextInt(50) + 1;
+				x[i].move(z);
+
+				if (x[i].getY() <= 100 + 10 * i) {
+					if (number[i] == i) {
+
+					} else {
+						count++;
+					}
+					number[i] = i;
+					x[i].setAngle(90);
+					x[i].setPenColor(255, 0, 0);
+					System.out.println(count);
+					if (count == 4) {
+						firstTurn = true;
+					}
+				}
+
+			}
+
 		}
-		else if(count==2) {
-			second=true;
-			i=11;
+		count = 0;
+		int secondnumber[] = { 0, 0, 0, 0, 0 };
+		while (firstTurn == true && secondTurn == false) {
+
+			for (int i = 0; i < x.length; i++) {
+				int z = new Random().nextInt(50) + 1;
+				x[i].move(z);
+
+				if (x[i].getX() >= 800 - 10 * i) {
+
+					if (secondnumber[i] == i) {
+
+					} else {
+						count++;
+					}
+					secondnumber[i] = i;
+					x[i].setAngle(180);
+					x[i].setPenColor(0, 255, 0);
+					System.out.println(count);
+					if (count == 4) {
+						secondTurn = true;
+					}
+
+				}
+
+			}
+
 		}
-		else if(count==3) {
-			third=true;
-			i=12;
+		count = 0;
+		int thirdnumber[] = { 0, 0, 0, 0, 0 };
+		while (firstTurn == true && secondTurn == true && thirdTurn == false) {
+			for (int i = 0; i < x.length; i++) {
+				int z = new Random().nextInt(50) + 1;
+				x[i].move(z);
+
+				if (x[i].getY() <= 500 - 10 * i) {
+					if (thirdnumber[i] == i) {
+
+					} else {
+						count++;
+					}
+					thirdnumber[i] = i;
+					x[i].setAngle(270);
+					x[i].setPenColor(0, 0, 255);
+					System.out.println(count);
+					if (count == 4) {
+						thirdTurn = true;
+					}
+				}
+
+			}
+
 		}
-		else if (count==4) {
-			fourth=true;
-			i=13;
-		}
-		count++;
-	}
-	if(o<x[i].getY()) {
-		o=x[i].getX();
-		slowest = i;
-	}
-	if(o<=20*slowest) {
-		firstTurn = true;
-	}
-	if(x[i].getY()>=249) {
-		i++;
-		JOptionPane.showMessageDialog(null, "Robot " + i + " has won!");
-		isOver = true;
-	}
-}
-}
+		/*
+		 * if (fourthTurn = true) { for (int i = 0; i < x.length; i++) { if (x[i].getY()
+		 * >= 250) { i++; JOptionPane.showMessageDialog(null, "Robot " + i +
+		 * " has won!"); isOver = true; } } }
+		 */
 		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
 		// screen.
 
